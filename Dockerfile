e92aae8eaab760d59734f240a8410e40445eb12f
Radu Potop <radu@wooptoo.com>
2022-04-15 10:42:48 +0100

    Initial commit of appendmail

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM python:3.10.4-slim-bullseye
+COPY . /opt/app
+WORKDIR /opt/app
+CMD python appendmail.py
---
+++ appendmail.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+#
+# Upload emails using IMAP append.
+
+import os
+from email.message import EmailMessage
+from imaplib import IMAP4, IMAP4_SSL, Time2Internaldate
+from time import time
+
+SERVER = os.getenv('IMAP_HOSTNAME', 'localhost')
+USERNAME = os.getenv('USERNAME', 'testuser')
+PASSWORD = os.getenv('PASSWORD', 'pass')
+MAILBOX = os.getenv('MAILBOX', 'INBOX')
+
+
+def auth() -> IMAP4:
+    mbox = IMAP4_SSL(SERVER)
+    mbox.login(USERNAME, PASSWORD)
+    return mbox
+
+
+def mbox_append(mbox: IMAP4, msg: EmailMessage):
+    return mbox.append(MAILBOX, None, Time2Internaldate(time()), msg.as_bytes())
+
+
+def read_emails():
+    """
+    Read emails from path.
+    """
+    pass
+
+
+def populate_emails():
+    mbox = auth()
+    emails = read_emails()
+    resp = tuple(mbox_append(mbox, eml) for eml in emails)
+    return emails, resp
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+    pprint(populate_emails())
