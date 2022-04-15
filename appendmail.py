21746060d308feffdbc6f17bf6910f4627e8179d
Radu Potop <radu@wooptoo.com>
2022-04-15 16:18:33 +0100

    Read emails from FS, add types

--- appendmail.py
+++ appendmail.py
@@ -5,12 +5,17 @@
 import os
 import sys
 from imaplib import IMAP4, IMAP4_SSL, Time2Internaldate
+from pathlib import Path
 from time import time
+from typing import Dict, Generator, Iterator, List, Tuple
 
 SERVER = os.getenv('IMAP_HOSTNAME')
 USERNAME = os.getenv('IMAP_USERNAME')
 PASSWORD = os.getenv('IMAP_PASSWORD')
 MAILBOX = os.getenv('IMAP_MAILBOX')
+
+Filename = str
+IMAPResult = Dict[Tuple, Filename]
 
 
 def auth() -> IMAP4:
@@ -28,18 +33,25 @@ def mbox_append(mbox: IMAP4, b_msg: bytes):
     return mbox.append(MAILBOX, None, Time2Internaldate(time()), b_msg)
 
 
-def read_emails():
+def read_emails_fs(path: str) -> Generator:
     """
     Read emails from path.
     """
-    return []
+    dir_iter = Path(path).iterdir()
+    try:
+        while _file := next(dir_iter):
+            yield (_file.read_bytes(), _file.name)
+    except StopIteration:
+        return None
 
 
-def populate_emails():
+def populate_emails(emails: Iterator) -> List[IMAPResult]:
     mbox = auth()
-    emails = read_emails()
-    resp = tuple(mbox_append(mbox, eml) for eml in emails)
-    return emails, resp
+    resp = list(
+        dict(result=mbox_append(mbox, b_msg), filename=filename)
+        for b_msg, filename in emails
+    )
+    return resp
 
 
 if __name__ == '__main__':
@@ -49,4 +61,4 @@ if __name__ == '__main__':
         print('Credentials not set')
         sys.exit(1)
 
-    pprint(populate_emails())
+    pprint(populate_emails(read_emails_fs('samples/')))
